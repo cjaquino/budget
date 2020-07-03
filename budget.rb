d8300d5087cfdfb49d079adb867c45a65acc4742
carl <jerritt.aquino@gmail.com>
2020-07-03 12:11:16 -0400

    first commit

---
+++ budget.rb
@@ -0,0 +1,17 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "sinatra/content_for"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  # session[:lists] ||= []
+end
+
+get "/" do
+  "Hello World"
+end
