9b3e82da96fa0d1f8dd987caf2121d16c6039306
carl <jerritt.aquino@gmail.com>
2020-07-03 18:37:23 -0400

    create overview screen with sample data

--- budget.rb
+++ budget.rb
@@ -1,5 +1,5 @@
 require "sinatra"
-require "sinatra/reloader" if development?
+require "sinatra/reloader" #if development?
 require "sinatra/content_for"
 require "tilt/erubis"
 
@@ -9,9 +9,56 @@ configure do
 end
 
 before do
-  # session[:lists] ||= []
+  # session[:budgets] ||= {}
+  session[:budgets] = { Vacation: [
+                          {
+                            name: 'breakfast',
+                            cost: 15,
+                            category: 'food'
+                          },
+                          {
+                            name: 'lunch',
+                            cost: 20,
+                            category: 'food'
+                          },
+                          {
+                            name: 'dinner',
+                            cost: 50,
+                            category: 'food'
+                          }
+                        ],
+                        Monthly: [
+                          {
+                            name: 'breakfast',
+                            cost: 15,
+                            category: 'food'
+                          },
+                          {
+                            name: 'lunch',
+                            cost: 20,
+                            category: 'food'
+                          },
+                          {
+                            name: 'dinner',
+                            cost: 50,
+                            category: 'food'
+                          }
+                        ]
+                       }
+end
+
+helpers do
+  def available(expenses)
+    expenses.map { |expense| expense[:cost] }.sum
+  end
 end
 
 get "/" do
-  redirect "/budget"
+  redirect "/budgets"
+end
+
+# Displays available budgets
+get "/budgets" do
+  @budgets = session[:budgets]
+  erb :overview, layout: :layout
 end
